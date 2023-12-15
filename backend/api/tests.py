7a8e6bc7897749436dfe8033c33d3acf4230e588
Pau Duran <pauduranmanzano@gmail.com>
2023-12-15 11:44:27 +0100

    Tests signup i login clients, treballadors i admins

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
+from django.urls import reverse, resolve
+from rest_framework import status
 
 from django.contrib.auth.models import User
 from .models import Usuari, Client, Treballador, Admin
@@ -103,3 +105,78 @@ class TestPermissions(TestCase):
         request.user = self.userClient
         permission = permissions.IsAdmin()
         self.assertFalse(permission.has_permission(request))
+
+
+class TestSignUpLogIn(TestCase):
+
+    def setUp(self) -> None:
+        self.email_client = "client@test.com"
+        self.email_treballador = "treballador@test.com"
+        self.email_admin = "admin@test.com"
+        self.username_client = "testClient"
+        self.username_treballador = "testTreballador"
+        self.username_admin = "testAdmin"
+        self.dni_client = "345"
+        self.dni_treballador = "456"
+        self.dni_admin = "567"
+        self.nom_client = "Client"
+        self.nom_treballador = "Treballador"
+        self.nom_admin = "Admin"
+        self.cognoms_client = "Client"
+        self.cognoms_treballador = "Treballador"
+        self.cognoms_admin = "Admin"
+        self.password = "contrasenya4321"
+
+    # Comprovació de Sign Up i Login d'un Client cas d'éxit
+    def test_successful_register_client(self):
+        # Sign Up Client
+        urlSignUp = reverse('signup_clients-list')
+        response = self.client.post(urlSignUp,
+                                    {"email": self.email_client, "username": self.username_client,
+                                     "dni": self.dni_client, "nom": self.nom_client, "cognoms": self.cognoms_client,
+                                     "password": self.password, "password2": self.password})
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+        # Login Client
+        urlLogIn = reverse('login_clients-list')
+        response2 = self.client.post(urlLogIn,
+                                     {"username": self.username_client, "password": self.password})
+        self.assertEquals(response2.status_code, status.HTTP_201_CREATED)
+
+        self.assertEquals(response.json()['token'], response2.json()['token'])
+
+    # Comprovació de Sign Up i Login d'un Treballador cas d'éxit
+    def test_successful_register_and_login_treballador(self):
+        # Sign Up Treballador
+        urlSignUp = reverse('signup_treballadors-list')
+        response = self.client.post(urlSignUp,
+                                    {"email": self.email_treballador, "username": self.username_treballador,
+                                     "dni": self.dni_treballador, "nom": self.nom_treballador, "cognoms": self.cognoms_treballador,
+                                     "password": self.password, "password2": self.password})
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+        # Login Treballador
+        urlLogIn = reverse('login_treballadors-list')
+        response2 = self.client.post(urlLogIn,
+                                     {"username": self.username_treballador, "password": self.password})
+        self.assertEquals(response2.status_code, status.HTTP_201_CREATED)
+
+        self.assertEquals(response.json()['token'], response2.json()['token'])
+
+    # Comprovació de Sign Up i Login d'un Admin cas d'éxit
+    def test_successful_register_and_login_admin(self):
+        # Sign Up Admin
+        urlSignUp = reverse('signup_admins-list')
+        response = self.client.post(urlSignUp,
+                                    {"email": self.email_admin, "username": self.username_admin,
+                                     "dni": self.dni_admin, "nom": self.nom_admin, "cognoms": self.cognoms_admin,
+                                     "password": self.password, "password2": self.password})
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+        # Login Admin
+        urlLogIn = reverse('login_admins-list')
+        response2 = self.client.post(urlLogIn,
+                                     {"username": self.username_admin, "password": self.password})
+        self.assertEquals(response2.status_code, status.HTTP_201_CREATED)
+
+        self.assertEquals(response.json()['token'], response2.json()['token'])
